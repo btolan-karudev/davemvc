eba33beb233da707cdf7137299bd2d1457d78b83
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-06-15 13:21:12 +0200

    Router and routing table ok

--- public/index.php
+++ public/index.php
@@ -16,4 +16,23 @@
 
 <?php
 
-echo '"'. $_SERVER['QUERY_STRING'] . '"';
+//echo '"'. $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
+
+//echo get_class($router);
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
