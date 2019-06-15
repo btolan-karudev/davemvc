06271322d20b87fec2288c3ae7e0097ddbcf1cf1
Beniamin Tolan <beniamin.tolan@outlook.com>
2019-06-15 14:06:31 +0200

    match method for searching in the routing table

--- Core/Router.php
+++ Core/Router.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
 class Router
 {
     /**
@@ -11,10 +10,16 @@ class Router
     protected $routes = [];
 
     /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $params = [];
+
+    /**
      * Add a route to the routing table
      *
-     * @param string $route  The route URL
-     * @param array  $params Parameters (controller, action, etc.)
+     * @param string $route The route URL
+     * @param array $params Parameters (controller, action, etc.)
      *
      * @return void
      */
@@ -31,6 +36,36 @@ class Router
     public function getRoutes()
     {
         return $this->routes;
+    }
+
+    /**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     *
+     * @return boolean  true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     *
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
     }
 
 }
